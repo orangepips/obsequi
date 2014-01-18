7ad1971b75ba8d63314ca0825887a69e89a76e01
Nathan Bullock <nathanbullock@gmail.com>
2014-01-18 07:37:44 -0500

    Improved upper bound on number of opponent moves.
    The old upper bound was basically:
    - number of squares - number of squares we will use = max moves for opponent.
    New upper bound is basically:
    - number of opponent moves - number of protected squares we will use = max
      moves for opponent.
    
    And then we take the min of these two upper bounds.
    
    Make very little difference of 8x8 board, significant on 8x9, reasonable on 9x9
    
    NEW
    solve rows 8 cols 9 V
    >> winner V, move (1,2), nodes 9,365,337.
    
    OLD:
    solve rows 8 cols 9 V
    >> winner V, move (1,2), nodes 14,814,479.
    
    NEW
    solve rows 9 cols 9 H
    >> winner H, move (2,1), nodes 1,401,296,346.
    
    OLD:
    solve rows 9 cols 9 H
    >> winner H, move (2,1), nodes 1,763,356,269.

--- obsequi/score-board.h
+++ obsequi/score-board.h
@@ -21,7 +21,7 @@ static inline bool is_game_over_expensive(
 
   score_board_curr_player(
       curr, &curr_lower_bound, &upper_bound, &upper_bound_real);
-  opp_upper_bound = upper_bound;
+  opp_upper_bound = MIN_TWO(upper_bound, upper_bound_real);
   if (curr_lower_bound > opp_upper_bound) {
     // current player wins.
     *score = 5000;
@@ -30,14 +30,32 @@ static inline bool is_game_over_expensive(
 
   score_board_next_player(
       opponent, &opp_lower_bound, &upper_bound, &upper_bound_real);
-  curr_upper_bound = upper_bound;
+  curr_upper_bound = MIN_TWO(upper_bound, upper_bound_real);
   if(opp_lower_bound >= curr_upper_bound) {
     // opponent wins.
     *score = -5000;
     return true;
   }
 
-  *score = 0;
+  //printf("%d -- %d:%d -- %d\n",
+  //       curr_lower_bound, curr_upper_bound, opp_lower_bound, opp_upper_bound);
+  int curr_avg = (curr_upper_bound + curr_lower_bound) / 2;
+  int opp_avg = (opp_upper_bound + opp_lower_bound) / 2;
+  if (curr_avg > opp_avg + 2) {
+    int avg_len = ((curr_upper_bound - curr_lower_bound) +
+                   (opp_upper_bound - opp_lower_bound)) / 2;
+    int overlap = (opp_upper_bound + 1) - curr_lower_bound;
+    //printf("X %d %d\n", overlap, avg_len);
+    *score = 100 - ((100 * overlap)/avg_len);
+  } else if (opp_avg > curr_avg + 2) {
+    int avg_len = ((curr_upper_bound - curr_lower_bound) +
+                   (opp_upper_bound - opp_lower_bound)) / 2;
+    int overlap = curr_upper_bound - opp_lower_bound;
+    //printf("Y %d %d\n", overlap, avg_len);
+    *score = -(100 - ((100 * overlap)/avg_len));
+  } else {
+    *score = curr_avg - opp_avg;
+  }
   //printf("score: %d\n", *score);
   return false;
 }
